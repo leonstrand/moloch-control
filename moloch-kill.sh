8241883d91ad676afd070b74e705ba576842b710
lstrand <leonstrand@gmail.com>
2016-07-27 10:34:36 -0700

    refactored to allow killing of individual components

--- moloch-kill.sh
+++ moloch-kill.sh
@@ -3,21 +3,51 @@
 # leon.strand@medeanalytics.com
 
 
-echo #verbose
-if ps -Fu daemon | grep -qv UID; then
-  echo $0: killing moloch... #verbose
-  echo #verbose
-  ps -Fu daemon
-  echo #verbose
-  for pid in $(ps -Fu daemon | grep -v UID | awk '{print $2}'); do
+execute() {
+  case "$1" in
+    n) shift;;
+    *) echo;; #verbose
+  esac
+  __command="$@"
+  echo $__command #verbose
+  eval $__command
+}
+
+pids=''
+if [ -n "$1" ]; then
+  components="$@"
+  for component in $components; do
+    case $component in
+      elasticsearch|viewer|capture) ;;
+      es|e) component='elasticsearch';;
+      v)    component='viewer';;
+      c)    component='capture';;
+      *)    echo #verbose
+            echo $0: warning: component $component unrecogized, skipping #verbose
+            continue
+      ;;
+    esac
+    echo #verbose
+    echo $0: getting pid of moloch component $component #verbose
+    pids=$pids' '$(ps -Fu daemon | grep -v UID | grep $component | awk '{print $2}')
+  done
+  pids=$(echo $pids | sed 's/ $//')
+else
+  pids=$(ps -Fu daemon | grep -v UID | awk '{print $2}')
+fi
+
+if [ -n "$pids" ]; then
+  for pid in $pids; do
     command='sudo kill '$pid
     echo $command #verbose
     eval $command
   done
   sleep 1
 else
+  echo #verbose
   echo $0: no moloch process to kill #verbose
 fi
+
 echo #verbose
 echo ps -Fu daemon #verbose
 ps -Fu daemon
